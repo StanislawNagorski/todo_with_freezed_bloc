7e2fe22fa801450a8abdf27b89ed5e20202c5a04
stah <stanislaw.nagorski@gmail.com>
2021-11-14 20:12:48 +0100

    freezed and hive added to models

--- lib/data/models/todo_task.dart
+++ lib/data/models/todo_task.dart
@@ -2,19 +2,18 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive/hive.dart';
 
 part 'todo_task.freezed.dart';
+
 part 'todo_task.g.dart';
 
 @freezed
 @HiveType(typeId: 1)
 class ToDoTask extends HiveObject with _$ToDoTask {
-
   @Assert('description.isNotEmpty', 'description cannot be empty')
-  factory ToDoTask({
-    @HiveField(0) required String id,
-    @HiveField(1) String description,
+  factory ToDoTask(
+    @HiveField(0) String id, {
+    @HiveField(1) required String description,
     @HiveField(2, defaultValue: false) @Default(false) bool isDone,
   }) = _ToDoTask;
 
-  ToDoTask.fromJson(Map<String, dynamic> json) => _$ToDoTask(json);
+  factory ToDoTask.fromJson(Map<String, dynamic> json) => _$ToDoTask(json);
 }
-
